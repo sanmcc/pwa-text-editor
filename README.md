0536772b753c9067e81aaf9ccd227a2e9efcbc43
sanmcc <120269286+sanmcc@users.noreply.github.com>
2023-06-04 22:05:25 -0600

    deleted develop folder

---
+++ README.md
@@ -0,0 +1,25 @@
+# PWA: Text Editor
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description 
+
+Text editor that runs in the browser as a single-page application that meets PWA criteria. The application will also function offline.
+- App home
+![Alt Text](./Develop/client/dist/assets/screenshot%202.JPG)
+- App icon on desktop
+![Alt Text](./Develop/client/dist/assets/screenshot%201.JPG)
+
+## User Story
+
+AS A developer
+I WANT to create notes or code snippets with or without an internet connection
+SO THAT I can reliably retrieve them for later use
+
+## Installation 
+- `npm i` 
+- `npm run build`
+- `npm run start`
+
+## Questions 
+[Email me](mailto:saniyya.mcclendon@gmail.com)
---
+++ client/src/js/database.js
@@ -0,0 +1,39 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({value: content});
+  const result = await request;
+  console.log('complete', result);
+  return result;
+};
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Showing all items from database', result);
+  return result.value;
+};
+
+
+initdb();
